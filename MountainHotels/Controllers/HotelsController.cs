6b0d55cdf5914e82158d67d04fc327f38c4f4264
Zoe Farrell <zoe@turing.edu>
2023-05-18 17:28:48 -0600

    Add create hotel form

--- MountainHotels/Controllers/HotelsController.cs
+++ MountainHotels/Controllers/HotelsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MountainHotels.DataAccess;
+using MountainHotels.Models;
 
 namespace MountainHotels.Controllers
 {
@@ -12,10 +13,29 @@ namespace MountainHotels.Controllers
             _context = context;
         }
 
+        // GET: /Hotels
         public IActionResult Index()
         {
             var hotels = _context.Hotels;
             return View(hotels);
+        }
+
+        // GET: /Hotels/New
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        // POST: /Hotels
+        [HttpPost]
+        public IActionResult Index(Hotel hotel)
+        {
+            //Take the movie sent in the request and save it to the database
+            _context.Hotels.Add(hotel);
+            _context.SaveChanges();
+
+            // Redirect back to the index page with all hotels
+            return RedirectToAction("Index");
         }
     }
 }
